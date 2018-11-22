78371f8fbe6e68abc17cdaf8456d761e9a3d2adf
Jonathan D. Lettvin <jlettvin@translations.com>
2018-11-22 02:18:30 -0500

    Much improved slides

--- theBBK/slides.js
+++ theBBK/slides.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function() {
-	var version = {major: 0, minor: 0, build: 13,};
+	var version = {major: 0, minor: 0, build: 15,};
 
 	// Re-use or build namespace
 	document.jlettvin = document.jlettvin || {};
@@ -30,50 +30,56 @@
 
 
 		swipedetect: function(el, callback) {
+			var touchsurface = el;
+			var swipedir;
+			var startX;
+			var startY;
+			var dx;
+			var dy;
+			var threshold = 150;   // required min distance considered swipe
+			var restraint = 100;   // maximum perpendicular distance
+			var allowedTime = 300; // maximum time allowed to travel that distance
+			var elapsedTime;
+			var startTime;
+			var handleswipe = callback || function(swipedir){};
 
-			var touchsurface = el,
-				swipedir,
-				startX,
-				startY,
-				distX,
-				distY,
-				threshold = 150, //required min distance traveled to be considered swipe
-				restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-				allowedTime = 300, // maximum time allowed to travel that distance
-				elapsedTime,
-				startTime,
-				handleswipe = callback || function(swipedir){}
-
-			touchsurface.addEventListener('touchstart', function(e){
+			touchsurface.addEventListener('touchstart', function(event) {
 				var touchobj = e.changedTouches[0]
 				swipedir = 'none'
 				dist = 0
 				startX = touchobj.pageX
 				startY = touchobj.pageY
-				startTime = new Date().getTime() // record time when finger first makes contact with surface
-				e.preventDefault()
-			}, false)
+				startTime = new Date().getTime() // time of first contact
+				event.preventDefault()
+			}, false);
 
-			touchsurface.addEventListener('touchmove', function(e){
-				e.preventDefault() // prevent scrolling when inside DIV
-			}, false)
+			touchsurface.addEventListener('touchmove', function(event) {
+				event.preventDefault() // prevent scrolling while swiping
+			}, false);
 
-			touchsurface.addEventListener('touchend', function(e){
-				var touchobj = e.changedTouches[0]
-				distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-				distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-				elapsedTime = new Date().getTime() - startTime // get time elapsed
-				if (elapsedTime <= allowedTime){ // first condition for awipe met
-					if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-						swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+			touchsurface.addEventListener('touchend', function(event) {
+				var touchobj = event.changedTouches[0]
+				dx = touchobj.pageX - startX // horizontal swipe displacement
+				dy = touchobj.pageY - startY // vertical   swipe displacement
+				elapsedTime = new Date().getTime() - startTime // elapsed time
+				// meet first condition for awipe
+				if (elapsedTime <= allowedTime) {
+					var adx = Math.abs(dx);
+					var ady = Math.abs(dy);
+					// meet 2nd condition for horizontal swipe
+					if (adx >= threshold && ady <= restraint) {
+						// if dist traveled is negative, it indicates left swipe
+						swipedir = (dx < 0)? 'left' : 'right'
 					}
-					else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-						swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+					// meet 2nd condition for vertical swipe
+					else if (ady >= threshold && adx <= restraint) {
+						// if dist traveled is negative, it indicates up swipe
+						swipedir = (dy < 0)? 'up' : 'down';
 					}
 				}
 				handleswipe(swipedir)
-				e.preventDefault()
-			}, false)
+				event.preventDefault()
+			}, false);
 		},
 
 		// Function to hide/show previous/next slide
@@ -153,6 +159,7 @@
 					case 'PageUp'  : case 'ArrowLeft' : slides.step(-1); break;
 					case 'PageDown': case 'ArrowRight': slides.step(+1); break;
 				}
+				event.preventDefault() // prevent scrolling when inside DIV
 			});
 
 			// Attach functions to swipe actions
