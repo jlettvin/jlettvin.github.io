16f089acb0d852ea7ddd53cf4d7417edb6c47804
Jonathan D. Lettvin <jlettvin@translations.com>
2018-11-22 02:36:01 -0500

    Much improved slides

--- theBBK/slides.js
+++ theBBK/slides.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function() {
-	var version = {major: 0, minor: 0, build: 15,};
+	const version = {major: 0, minor: 0, build: 16,};
 
 	// Re-use or build namespace
 	document.jlettvin = document.jlettvin || {};
@@ -9,7 +9,7 @@
 		version: version,
 		body:    document.getElementsByTagName("body")[0],
 		article: document.getElementsByTagName("article")[0],
-		buttons: document.getElementsByTagName("nav")[0],
+		nav: document.getElementsByTagName("nav")[0],
 		section: document.getElementsByTagName("section"),
 		button: [],
 		style: {
@@ -24,31 +24,28 @@
 		},
 		show: 0,
 
-		xDown: null,
-		yDown: null,
-
-
-
 		swipedetect: function(el, callback) {
-			var touchsurface = el;
+			var handleswipe    = callback || function(swipedir){};
+			var touchsurface   = el;
+
+			const threshold    = 150; // required min distance considered swipe
+			const restraint    = 100; // maximum perpendicular distance
+			const allowedTime  = 300; // maximum time for swipe
+
 			var swipedir;
-			var startX;
-			var startY;
+			var x0;
+			var y0;
 			var dx;
 			var dy;
-			var threshold = 150;   // required min distance considered swipe
-			var restraint = 100;   // maximum perpendicular distance
-			var allowedTime = 300; // maximum time allowed to travel that distance
 			var elapsedTime;
 			var startTime;
-			var handleswipe = callback || function(swipedir){};
 
 			touchsurface.addEventListener('touchstart', function(event) {
 				var touchobj = e.changedTouches[0]
 				swipedir = 'none'
 				dist = 0
-				startX = touchobj.pageX
-				startY = touchobj.pageY
+				x0 = touchobj.pageX
+				y0 = touchobj.pageY
 				startTime = new Date().getTime() // time of first contact
 				event.preventDefault()
 			}, false);
@@ -59,8 +56,8 @@
 
 			touchsurface.addEventListener('touchend', function(event) {
 				var touchobj = event.changedTouches[0]
-				dx = touchobj.pageX - startX // horizontal swipe displacement
-				dy = touchobj.pageY - startY // vertical   swipe displacement
+				dx = touchobj.pageX - x0 // horizontal swipe displacement
+				dy = touchobj.pageY - y0 // vertical   swipe displacement
 				elapsedTime = new Date().getTime() - startTime // elapsed time
 				// meet first condition for awipe
 				if (elapsedTime <= allowedTime) {
@@ -69,12 +66,12 @@
 					// meet 2nd condition for horizontal swipe
 					if (adx >= threshold && ady <= restraint) {
 						// if dist traveled is negative, it indicates left swipe
-						swipedir = (dx < 0)? 'left' : 'right'
+						swipedir = (dx < 0)? 'SwipeLeft' : 'SwipeRight'
 					}
 					// meet 2nd condition for vertical swipe
 					else if (ady >= threshold && adx <= restraint) {
 						// if dist traveled is negative, it indicates up swipe
-						swipedir = (dy < 0)? 'up' : 'down';
+						swipedir = (dy < 0)? 'SwipeUp' : 'SwipeDown';
 					}
 				}
 				handleswipe(swipedir)
@@ -126,7 +123,7 @@
 				this.version.minor + '.' +
 				this.version.build + '<br />';
 			version.setAttribute("style", "font-size: 10px; padding: 0px 20px 0px 0px");
-			this.buttons.appendChild(version);
+			this.nav.appendChild(version);
 
 			// Accumulate the slides for display
 			for (var N = this.counted, n = 0; n < N; ++n) {
@@ -143,30 +140,32 @@
 				abbr.setAttribute("visibility", "hidden");
 				button.innerHTML = 'Slide' + (n + 1);
 				button.onclick = this.jump;
-				buttons.setAttribute("style", this.style.button[0]);
+				this.nav.setAttribute("style", this.style.button[0]);
 				abbr.appendChild(button);
-				this.buttons.appendChild(abbr);
+				this.nav.appendChild(abbr);
 				this.button.push(button);
 			}
 
 			// Do initial choice and exchange
 			this.step(0);
 
-			// Attach functions to keyboard input
-			var slides  = this;  // Needed for context
+			// Attach function to keyboard input
+			var slides  = document.jlettvin.slides;  // Needed for context
 			document.addEventListener('keyup', function(event) {
 				switch(event.code) {
-					case 'PageUp'  : case 'ArrowLeft' : slides.step(-1); break;
-					case 'PageDown': case 'ArrowRight': slides.step(+1); break;
+					case 'PageUp'  :
+					case 'ArrowLeft':  case 'ArrowUp':   slides.step(-1); break;
+					case 'PageDown':
+					case 'ArrowRight': case 'ArrowDown': slides.step(+1); break;
 				}
-				event.preventDefault() // prevent scrolling when inside DIV
-			});
+				event.preventDefault() // prevent scrolling
+			}, false);
 
-			// Attach functions to swipe actions
+			// Attach function to swipe actions
 			this.swipedetect(slides.body,function(swipedir) {
 				switch(swipedir) {
-					case  'left': case   'up': slides.step(+1); break;
-					case 'right': case 'down': slides.step(-1); break;
+					case  'SwipeLeft': case   'SwipeUp': slides.step(+1); break;
+					case 'SwipeRight': case 'SwipeDown': slides.step(-1); break;
 				};
 			});
 
