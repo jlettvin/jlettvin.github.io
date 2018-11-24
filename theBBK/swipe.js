4fe41ad644da3d6e6f2a6b4fd5f3027f6fd72533
Jonathan D. Lettvin <jlettvin@translations.com>
2018-11-23 22:29:11 -0500

    favicon.ico

--- theBBK/swipe.js
+++ theBBK/swipe.js
@@ -8,7 +8,53 @@
 	document.jlettvin.swipe = document.jlettvin.swipe || {
 		version: version,  // this javascript code version
 
-		swipe: function(el, callback) {
+		swipe: function(el,func) {
+			var swipe_det = new Object();
+			swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+			var min_x = 30;  //min x swipe for horizontal swipe
+			var max_x = 30;  //max x difference for vertical swipe
+			var min_y = 50;  //min y swipe for vertical swipe
+			var max_y = 60;  //max y difference for horizontal swipe
+			var direc = "";
+			var ele = el;
+			//var ele = document.getElementById(el);
+			ele.addEventListener('touchstart',function(e){
+				var t = e.touches[0];
+				swipe_det.sX = t.screenX; 
+				swipe_det.sY = t.screenY;
+			},false);
+			ele.addEventListener('touchmove',function(e){
+				e.preventDefault();
+				var t = e.touches[0];
+				swipe_det.eX = t.screenX; 
+				swipe_det.eY = t.screenY;    
+			},false);
+			ele.addEventListener('touchend',function(e){
+				//horizontal detection
+				if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+					if(swipe_det.eX > swipe_det.sX) direc = "r";
+					else direc = "l";
+				}
+				//vertical detection
+				else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+					if(swipe_det.eY > swipe_det.sY) direc = "d";
+					else direc = "u";
+				}
+
+				if (direc != "") {
+					if(typeof func == 'function') func(el,direc);
+				}
+				direc = "";
+				swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+			},false);  
+			return 'returned from call to new swipe handler installer';
+		},
+
+		//function myfunction(el,d) {
+			//alert("you swiped on element with id '"+el+"' to "+d+" direction");
+		//}
+
+		oldswipe: function(el, callback) {
 			var handleswipe    = callback || function(swipedir){};
 			var touchsurface   = el;
 
@@ -65,7 +111,7 @@
 				event.preventDefault()
 			}, false);
 
-			return 'returned from call to swipe handler installer';
+			return 'returned from call to old swipe handler installer';
 		},
 	};
 
