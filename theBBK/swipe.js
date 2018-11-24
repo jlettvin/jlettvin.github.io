38ba738a1200ec471507961d7eef88fabdeef64b
Jonathan D. Lettvin <jlettvin@translations.com>
2018-11-24 01:01:14 -0500

    swipe

--- theBBK/swipe.js
+++ theBBK/swipe.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function() {
-	const version = {major: 0, minor: 0, build: 25,};
+	const version = {major: 0, minor: 0, build: 26,};
 	const verstr  = '' + version.major + '.' + version.minor + '.' + version.build;
 
 	// Re-use or build namespace
@@ -21,58 +21,16 @@
 				document.jlettvin.swipe.version + ' [' + title + ']: ' + msg;
 		},
 
-		newswipe: function(el,func) {
-			var swipe_det = new Object();
-			swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
-			var min_x = 30;  //min x swipe for horizontal swipe
-			var max_x = 30;  //max x difference for vertical swipe
-			var min_y = 50;  //min y swipe for vertical swipe
-			var max_y = 60;  //max y difference for horizontal swipe
-			var direc = "";
-			var ele = el;
-			//var ele = document.getElementById(el);
-			ele.addEventListener('touchstart',function(e){
-				var t = e.touches[0];
-				swipe_det.sX = t.screenX; 
-				swipe_det.sY = t.screenY;
-			},false);
-			ele.addEventListener('touchmove',function(e){
-				e.preventDefault();
-				var t = e.touches[0];
-				swipe_det.eX = t.screenX; 
-				swipe_det.eY = t.screenY;    
-			},false);
-			ele.addEventListener('touchend',function(e){
-				//horizontal detection
-				if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
-					if(swipe_det.eX > swipe_det.sX) direc = "r";
-					else direc = "l";
-				}
-				//vertical detection
-				else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
-					if(swipe_det.eY > swipe_det.sY) direc = "d";
-					else direc = "u";
-				}
-
-				if (direc != "") {
-					if(typeof func == 'function') func(el,direc);
-				}
-				direc = "";
-				swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
-			},false);  
-			return 'returned from call to new swipe handler installer';
-		},
-
-		//function myfunction(el,d) {
-			//alert("you swiped on element with id '"+el+"' to "+d+" direction");
-		//}
-
 		swipe: function(el, callback) {
 			var handleswipe    = callback || function(swipedir){};
 			var touchsurface   = el;
 
+			//-------------------------------------------------------------------
 			touchsurface.addEventListener('touchstart', function(event) {
 				var my = document.jlettvin.swipe;
+
+				my.show('event', event);
+
 				var touched = e.touches[0];
 				my.swipedir = null;
 				my.x0 = touched.screenX;
@@ -82,12 +40,14 @@
 				event.preventDefault()
 			}, false);
 
+			//-------------------------------------------------------------------
 			touchsurface.addEventListener('touchmove', function(event) {
 				var my = document.jlettvin.swipe;
 				my.show('move', '');
 				event.preventDefault(); // prevent scrolling while swiping
 			}, false);
 
+			//-------------------------------------------------------------------
 			touchsurface.addEventListener('touchend', function(event) {
 				var my = document.jlettvin.swipe;
 				var touched = event.touches[0];
@@ -130,8 +90,7 @@
 				if(my.swipedir != null) handleswipe(my.swipedir)
 				event.preventDefault()
 			}, false);
-
-			return this.version;
+			//-------------------------------------------------------------------
 		},
 	};
 
